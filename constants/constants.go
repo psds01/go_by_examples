4091331c51951fbfb738a5096698e4046257bedd
nilesh <nilesh.chaudhari@paysense.in>
2019-09-20 02:00:38 +0530

    add comments

--- constants/constants.go
+++ constants/constants.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+// CONST can be used wherever VAR can be.
+
 // CONST varName TYPE = value
 const myConstantString string = "CONSTANT_STRING"
 
@@ -13,10 +15,14 @@ func main() {
 	// CONST varName = value
 	const bigNumber = 500000000
 	fmt.Println("Big number", bigNumber)
+	// CONST expressions perform arithematics operations with arbitrary proportion.
 	const derivedBigNumber = 3e20 / bigNumber
-	fmt.Println("Derived Big Number, scientific", derivedBigNumber)
-	fmt.Println("Derived Big Number, int", int64(derivedBigNumber))
+	fmt.Println("Derived Big Number, scientific, arbitrary precision", derivedBigNumber)
+	fmt.Println("Derived Big Number, int, type given", int64(derivedBigNumber))
 
+	// TYPE assignment by context: bcoz math.Sin expects FLOAT, bigNumber get TYPE float
 	fmt.Println(math.Sin(bigNumber))
+
+	// VAR === CONST
 
 }
