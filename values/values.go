861f85cdcdbd53618fbe0f550def67e3a1feb7e9
nilesh <nilesh.chaudhari@paysense.in>
2019-09-19 22:35:54 +0530

    float and bool ops

--- values/values.go
+++ values/values.go
@@ -6,6 +6,7 @@ import "fmt"
 
 func main() {
 	// string ops
+	fmt.Println("\n\nString operations:--->")
 
 	// prints "golang"; ends with newline
 	fmt.Println("go" + "lang")
@@ -21,10 +22,35 @@ func main() {
 	fmt.Printf("jau kaha\n")
 
 	// int ops
+	fmt.Println("\n\nInteger operations:--->")
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("7-3 =", 7-3)
 	fmt.Println("1-1 =", 1-1)
+	// int * int = int
 	fmt.Println("12*4 =", 12*4)
+	// int / int = int
 	fmt.Println("20/4 = ", 20/4)
+
+	// float ops
+	fmt.Println("\n\nFloat operations:--->")
+	// int / float = float
+	fmt.Println("4/5.0 = ", 4/5.0)
+	//float / int = float
+	fmt.Println("4.0/5 = ", 4.0/5)
+	// float / int = float
+	fmt.Println("With `fmt.Println` -> 22.0/7.0 = ", 22.0/7.0)
+	// WTF? is fmt a specialized package for printing?
+	// float / float = float
+	println("With `built-in println` -> 22.0/7.0 = ", 22.0/7.0)
+	// fmt.Println("22.0/7.0 =", 22.0/7.0)
+
+	// bool ops
+	fmt.Println("\n\nBoolean operations:--->")
+	// && = and
+	fmt.Println("true && false =", true && false)
+	// || = or
+	fmt.Println("true || false =", true || false)
+	// ! = not
+	fmt.Println("!false =", !false)
 
 }
