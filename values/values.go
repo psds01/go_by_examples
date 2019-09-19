8eaacb58f7914dfb7c5726d101fe00c540547e6b
nilesh <nilesh.chaudhari@paysense.in>
2019-09-19 22:07:42 +0530

    string types

--- values/values.go
+++ values/values.go
@@ -5,5 +5,11 @@ package main
 import "fmt"
 
 func main() {
+	// prints "golang"; the line endswith newline
 	fmt.Println("go" + "lang")
+	// prints "golang"; no newline at the end
+	fmt.Print("go" + "lang")
+	// no newline at the end
+	fmt.Printf(" is" + " good")
+
 }
