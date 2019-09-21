180d6235c9cfc02691aa19b6d8b84ecd23f41c05
nilesh <nilesh.chaudhari@paysense.in>
2019-09-21 15:30:46 +0530

    if -- else if --else

--- if-else/if_else.go
+++ if-else/if_else.go
@@ -13,4 +13,15 @@ func main() {
 		fmt.Println("8 is divisible by 4")
 	}
 
+	// The scope of this variale is just for the if...else part
+	if num := 11; num%2 == 0 {
+		fmt.Println("11 is divisible by 2")
+	} else if num%5 == 0 {
+		fmt.Println("11 is divisible by 5")
+	} else {
+		fmt.Println("11 is divisible neither by 2 nor by 5")
+	}
+	// undefined: num
+	// fmt.Println(num)
+
 }
