66dc8c87463ffb7e344cc4cbd47c7cf79625dd5a
nilesh <nilesh.chaudhari@paysense.in>
2019-09-22 13:49:37 +0530

    set to and get from slice

--- slices/slices.go
+++ slices/slices.go
@@ -14,7 +14,23 @@ func main() {
 	// mySlice := make([]TYPE, LENGTH)
 	// LENGTH is the len of the slice but it's not TYPEd by len
 	// The type of the slice is `[]INT`
-	stringSlice := make([]string, 3)
+	stringSlice := make([]string, 7)
 	fmt.Println(stringSlice)
+
+	fmt.Println("Setting elements to slice:", stringSlice)
+	// set values::
+	// syntax : slice[index] = value
+	stringSlice[0] = "f"
+	stringSlice[1] = "e"
+	stringSlice[2] = "r"
+	stringSlice[3] = "r"
+	stringSlice[4] = "a"
+	stringSlice[5] = "r"
+	stringSlice[6] = "i"
+	fmt.Println("All done. Here's the slice:", stringSlice)
+
+	// get elements by index
+	// syntax: slice[index]
+	fmt.Println("The element at index 4 is", stringSlice[4])
 
 }
