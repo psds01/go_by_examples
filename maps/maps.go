2376f889a92d7b6cc4464705b3ec3c3afb6594b8
nilesh <nilesh.chaudhari@paysense.in>
2019-09-22 17:30:41 +0530

    set values in map

--- maps/maps.go
+++ maps/maps.go
@@ -12,4 +12,13 @@ func main() {
 	// syntax : mapName := MAKE(MAP[keyType][valType])
 	carToPriceMap := make(map[string]int)
 	fmt.Println(carToPriceMap)
+
+	// set values
+	// syntax: mapName[key] = value
+	carToPriceMap["ferrari"] = 300000
+	carToPriceMap["lamborghini"] = 250000
+	carToPriceMap["bmw"] = 150000
+	carToPriceMap["mercedes"] = 125000
+	fmt.Println("Prices of cars in USD:", carToPriceMap)
+
 }
