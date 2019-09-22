a97521842dce034b39be2f011aa7187b1d94b396
nilesh <nilesh.chaudhari@paysense.in>
2019-09-22 12:39:48 +0530

    define empty arrays: int, float, string, bool

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -8,7 +8,20 @@ import "fmt"
 func main() {
 	// define a variable `arr` of type `[5]int`
 	// length and type of elements are type of that array
-	var arr [5]int
+	var intArr [4]int
 	// prints array with 5 `empty int` values i.e. 0
-	fmt.Println(arr)
+	fmt.Println("int empty arr:", intArr)
+
+	// empty array of bool
+	var boolArr [2]bool
+	fmt.Println("bool empty arr:", boolArr)
+
+	// empty arr of floats
+	var floatArr [3]float64
+	fmt.Println("float empty arr:", floatArr)
+
+	// empty array of elements with type string
+	var stringArr [10]string
+	fmt.Println(stringArr)
+
 }
