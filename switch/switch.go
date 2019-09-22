23ec4ff3327aa79e348eb2dc9fd04d9183a64eaa
nilesh <nilesh.chaudhari@paysense.in>
2019-09-22 12:16:03 +0530

    switch without an expression

--- switch/switch.go
+++ switch/switch.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 
+	// simple switch
 	car := "ferrari"
 	fmt.Print("I want ")
 	switch car {
@@ -17,12 +18,24 @@ func main() {
 		fmt.Println("ferrari!!!")
 	}
 
+	// switch with default
 	num := 7
 	switch num % 2 {
 	case 0:
 		fmt.Println(num, "is even")
 	default:
 		fmt.Println(num, "is odd")
+	}
+
+	// switch without an expression
+	whatDoWeWant := "car"
+	switch {
+	case whatDoWeWant == "bike":
+		fmt.Println("No, bike is cheap!!")
+	case whatDoWeWant == "private jet":
+		fmt.Println("Well, I won't mind!!")
+	case whatDoWeWant == "car":
+		fmt.Println("Oh, yeah... a car")
 	}
 
 }
