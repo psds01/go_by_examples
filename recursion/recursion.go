f1295235f848a20d04dc36b2021b828f86d96736
nilesh <nilesh.chaudhari@paysense.in>
2019-09-23 22:04:13 +0530

    fibonacci

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -9,6 +9,16 @@ func fact(n int) int {
 	return n * fact(n-1)
 }
 
+func fibonacci(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
 func main() {
 	fmt.Println("Printing factorial of 7:", fact(7))
+	for i := 0; i < 10; i++ {
+		fmt.Println("Fibonacci series at index", i, ":", fibonacci(i))
+	}
 }
