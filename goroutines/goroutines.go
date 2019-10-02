fa2f5fe5d5f087fab9001971dbc4d293b47f4091
nilesh <nilesh.chaudhari@paysense.in>
2019-10-03 01:47:16 +0530

    ??

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
 
 func main() {
 	fmt.Println("Go routines!")
+	f("basic")
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
 }
